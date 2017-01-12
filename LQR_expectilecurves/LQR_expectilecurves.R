942baeb422ed8fabb1808a6dc70227e087d2ba6f
Chen Huang <chen.huang@hu-berlin.de>
2017-01-12 12:34:08 +0100

    update LQR_expectilecurves

--- LQR_expectilecurves/LQR_expectilecurves.R
+++ LQR_expectilecurves/LQR_expectilecurves.R
@@ -3,40 +3,51 @@
 rm(list = ls(all = TRUE))
 graphics.off()
 
-## install and load packages
-libraries = c("expectreg")
-lapply(libraries, function(x) if (!(x %in% installed.packages())) {
-    install.packages(x)
-})
-lapply(libraries, library, quietly = TRUE, character.only = TRUE)
-
-## generating data from AR(1)
+## generating data
 n = 100
 m = 10
 y = 0
-a = 0.7
+c = 0.8
 set.seed(2017)
 for (j in 1:m) {
-    innov = rnorm(n, mean = 0, sd = 1)
-    x     = rep(0, n)
-    x[1]  = innov[1]
-    for (i in 2:n) {
-        x[i] = a * x[i - 1] + innov[i]
-    }
-    y = cbind(y, x)
+    innov = rnorm(n, mean = 0, sd = 0.5)
+    xx    = seq(0, 1, length = n)
+    y     = cbind(y, c * sin(2 * pi * xx) + innov)
 }
 y = y[, -1]
-plot(y[, 1], ylab = "", xlab = "", type = "l", col = "white", ylim = c(min(y), max(y)))
+plot(xx, y[, 1], ylab = "", xlab = "", type = "l", col = "white", ylim = c(min(y), 
+    max(y)))
 ## plot the curves
 for (j in 1:m) {
-    lines(y[, j])
+    lines(xx, y[, j])
 }
+
+## LAWS for vector, return (1-dim)expectile at given tau level
+laws1D = function(vec0, tau0) {
+    n  = length(vec0)
+    e0 = mean(vec0)
+    repeat {
+        i_pos = vec0 >= e0
+        n_pos = sum(i_pos)
+        i_neg = vec0 < e0
+        n_neg = sum(i_neg)
+        e0    = as.numeric((tau0 * (i_pos %*% vec0) + (1 - tau0) * (i_neg %*% vec0))/(sum(tau0 * 
+            i_pos) + sum((1 - tau0) * i_neg)))
+        if (all((vec0 >= e0) == i_pos)) {
+            break
+        }
+    }
+    return(e0)
+}
+
 ## compute and plot the expectile curves
-e = 0
-for (v in 1:n) {
-    e = rbind(e, expectile(y[v, ], c(0.1, 0.5, 0.9)))
+tau = c(0.1, 0.5, 0.9)
+e   = matrix(0, nrow = n, ncol = length(tau))
+for (i in 1:n) {
+    for (t in 1:length(tau)) {
+        e[i, t] = laws1D(y[i, ], tau[t])
+    }
 }
-e = e[-1, ]
-lines(e[, 1], col = "blue", lwd = 3)
-lines(e[, 2], col = "darkgreen", lwd = 3)
-lines(e[, 3], col = "red", lwd = 3)
+lines(xx, e[, 1], lwd = 3, col = "blue")
+lines(xx, e[, 2], lwd = 3, col = "darkgreen")
+lines(xx, e[, 3], lwd = 3, col = "red")
